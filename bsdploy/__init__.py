11e9d6049ac7abd5905a09bff097b95c5cccc7a7
Tom Lazar <tomster@pyfidelity.com>
2014-03-20 15:38:51 +0100

    expose applying the configuration playbook as ploy command

--- bsdploy/__init__.py
+++ bsdploy/__init__.py
@@ -23,7 +23,7 @@ class PloyBootstrapCmd(object):
         self.aws = aws
 
     def __call__(self, argv, help):
-        """Bootstrap a jailhost"""
+        """Bootstrap a jailhost that's been booted into MFSBsd."""
         parser = argparse.ArgumentParser(
             prog="ploy bootstrap",
             description=help,
@@ -40,9 +40,35 @@ class PloyBootstrapCmd(object):
         instance = self.aws.instances[args.master[0]]
         instance.do('bootstrap')
 
+
+class PloyConfigureHostCmd(object):
+
+    def __init__(self, aws):
+        self.aws = aws
+
+    def __call__(self, argv, help):
+        """Configure a jailhost (installs ezjail etc.) after it has been bootstrapped."""
+        parser = argparse.ArgumentParser(
+            prog="ploy configure-jailhost",
+            description=help,
+            add_help=False,
+        )
+        masters = dict((master.id, master) for master in self.aws.get_masters('ezjail_admin'))
+        parser.add_argument(
+        	"master",
+        	nargs=1,
+	        metavar="master",
+	        help="Name of the jailhost from the config.",
+	        choices=masters)
+        args = parser.parse_args(argv[:1])
+        instance = self.aws.instances[args.master[0]]
+        instance.apply_playbook(path.join(ploy_path, 'roles', 'jailhost.yml'))
+
 def get_commands(aws):
     return [
-        ('bootstrap', PloyBootstrapCmd(aws))]
+        ('bootstrap', PloyBootstrapCmd(aws)),
+        ('configure-jailhost', PloyConfigureHostCmd(aws)),
+        ]
 
 
 plugin = dict(
