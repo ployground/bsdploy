3572bd3aac928e2ac04923aca0c7feff5257654a
Tom Lazar <tomster@pyfidelity.com>
2014-05-08 21:33:17 +0200

    rename -> otherwise we cannot import anything from Fabric...

--- bsdploy/fabfile.py
+++ bsdploy/fabfile.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 from fabric.api import env
-from bsdploy.fabric import bootstrap, fetch_assets, bootstrap_daemonology
+from bsdploy.fabrics import bootstrap, fetch_assets, bootstrap_daemonology
 
 # a plain, default fabfile for jailhosts
 
