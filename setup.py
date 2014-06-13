37c61e0734bd327fa90e66b4c44ca8095d24397b
Tom Lazar <tomster@pyfidelity.com>
2014-06-13 15:53:46 +0200

    prepare next alpha

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-version = "0.1a2"
+version = "0.1a3"
 
 setup(
     version=version,
