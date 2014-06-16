7dc3064b42047800f3b4c08ddb0b784eec30e41a
Tom Lazar <tomster@pyfidelity.com>
2014-06-16 23:16:10 +0200

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 
-version = "0.1a4"
+version = "0.1a5"
 
 setup(
     version=version,
