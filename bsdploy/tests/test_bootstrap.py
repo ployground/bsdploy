f49c41c84246b9dd35658f480976efee0c20f2d5
Tom Lazar <tomster@pyfidelity.com>
2014-06-21 19:17:03 +0200

    first test(!)

--- bsdploy/tests/test_bootstrap.py
+++ bsdploy/tests/test_bootstrap.py
@@ -1,2 +1,8 @@
-def test_foo():
-    pass
+from mock import patch
+from bsdploy import bootstrap_utils as bu
+
+
+def test_get_realmem():
+    with patch('bsdploy.bootstrap_utils.run') as mocked_run:
+        mocked_run.return_value = '536805376'
+        assert bu.get_realmem() == 512
