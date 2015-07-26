36c6388b6f7d39ea09ed5c3e52262128fe1e7e6c
Tom Lazar <tomster@pyfidelity.com>
2015-07-26 13:29:15 +0200

    update to latest releases

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ install_requires = [
     'PyYAML',
     'jinja2',
     'setuptools',
-    'ploy>=1.1.0',
-    'ploy_ansible>=1.2.4',
-    'ploy_ezjail>=1.1.0',
+    'ploy>=1.2.0',
+    'ploy_ansible>=1.3.0',
+    'ploy_ezjail>=1.2.0',
     'ploy_fabric>=1.1.0',
 ]
 
