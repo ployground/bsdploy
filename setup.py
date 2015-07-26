142bf748dac5df4f79742f62414b0c243d0926e2
Tom Lazar <tomster@pyfidelity.com>
2015-07-26 13:33:50 +0200

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 
-version = "2.1.0"
+version = "2.2.0dev"
 
 install_requires = [
     'PyYAML',
